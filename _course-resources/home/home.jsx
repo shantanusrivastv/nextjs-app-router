ba0b7f8cf47a5d4ec6d457577244502d580b3341
Jim Cooper <jim@cooperplanet.com>
2024-11-05 11:31:22 -0700

    Starting point

---
+++ _course-resources/home/home.jsx
@@ -0,0 +1,23 @@
+<>
+  <div className={styles.heroImage}>
+    <Image src={heroBanner} fill alt="" />
+  </div>
+  <div className={styles.cta}>
+    <Image style={{ marginLeft: '10px' }} height=" 200" width="200" src={divinity} alt="" />
+    <Image height="200" width="200" src={synchronicity} alt="" />
+    <div className={styles.ctaText}>
+      <div>
+        Your <em>Album</em> and <em>Artist</em> Name
+      </div>
+      <div className={styles.ctaSubText}>
+        <div>on <em>Custom</em> Album Designs</div>
+      </div>
+      <button className="buton cta">Shop Now</button>
+    </div>
+    <Image height="200" width="200" src={theUndertaking} alt="" />
+    <Image height="200" width="200" src={asTheSunSpeaks} alt="" />
+  </div>
+  <div className={styles.soundwavesImage}>
+    <Image src={soundwaves} alt='' />
+  </div>
+</>

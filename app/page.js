d21beb3dc3620080e960cbb1a8f862d9611a07a9
Kumar Shantanu <shantanusrivastv@gmail.com>
2025-08-12 13:28:38 +0530

    Initial with css applied to root page.js

--- app/page.js
+++ app/page.js
@@ -1,95 +1,39 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import heroBanner from '../public/images/hero-banner.png';
+import{
+divinity,
+synchronicity,
+theUndertaking,
+asTheSunSpeaks
+} from '../public/images/albums';
+
+import soundwaves from '../public/images/soundwaves.jpg';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+	  <>
+		  <div className={styles.heroImage}>
+			  <Image src={heroBanner} fill alt="" />
+		  </div>
+		  <div className={styles.cta}>
+			  <Image style={{ marginLeft: '10px' }} height=" 200" width="200" src={divinity} alt="" />
+			  <Image height="200" width="200" src={synchronicity} alt="" />
+			  <div className={styles.ctaText}>
+				  <div>
+					  Your <em>Album</em> and <em>Artist</em> Name
+				  </div>
+				  <div className={styles.ctaSubText}>
+					  <div>on <em>Custom</em> Album Designs</div>
+				  </div>
+				  <button className="buton cta">Shop Now</button>
+			  </div>
+			  <Image height="200" width="200" src={theUndertaking} alt="" />
+			  <Image height="200" width="200" src={asTheSunSpeaks} alt="" />
+		  </div>
+		  <div className={styles.soundwavesImage}>
+			  <Image src={soundwaves} alt='' />
+		  </div>
+	  </>
   );
 }

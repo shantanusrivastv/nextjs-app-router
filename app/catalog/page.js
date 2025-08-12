3862c5369f1620907681188bba86e685da418600
Kumar Shantanu <shantanusrivastv@gmail.com>
2025-08-13 00:19:34 +0530

    using server component and simulate a DB call by using node fs

--- app/catalog/page.js
+++ app/catalog/page.js
@@ -1,11 +1,16 @@
 import styles from './catalog.module.css';
-import products from './products.json';
+//import products from './products.json';
+import { promises as fs } from 'fs';
+import path from 'path';
 import ProductDetails from './ProductDetails';
 
 export default async function Catalog() {
-
 	//Simulating a slow page
-	await new Promise((resolve) => setTimeout(resolve, 1500));
+	await new Promise((resolve) => setTimeout(resolve, 500));
+
+	const filePath = path.join(process.cwd(), 'app/catalog/products.json')
+	const fileContents = await fs.readFile(filePath, 'utf8')
+	const products = JSON.parse(fileContents)
 
 	return (
 		<div>
